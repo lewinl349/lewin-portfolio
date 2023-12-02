dc2dffdaee361d7f43580e581c4bfc5c8cfb2274
lewinl349 <118216725+lewinl349@users.noreply.github.com>
2023-12-02 06:32:03 +0000

    Fix directories

--- app/ui/sidebar.tsx
+++ app/ui/sidebar.tsx
@@ -1,9 +1,9 @@
 export default function Sidebar() {
     return (
-    <div className="col-span-2 p-4 border rounded shadow-md hover:bg-sky-100">
+    <div className="col-span-2 p-4 border rounded shadow-xl hover:bg-sky-200">
     <nav>
       <a href="/">Home</a> <br></br>
-      <a href="/projects/">Projects</a> <br></br>
+      <a href="/lewin-portfolio/projects/">Projects</a> <br></br>
       {/*<a href="/resume/">Resume</a> <br></br>
       <a href="/contact/">Contact Me</a> */}
     </nav>
