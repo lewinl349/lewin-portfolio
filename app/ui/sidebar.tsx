e151dccd9632da0c14c65bb8a17b8bd038a25b5a
lewinl349 <118216725+lewinl349@users.noreply.github.com>
2023-12-02 19:20:45 +0000

    Get rid of slash in path

--- app/ui/sidebar.tsx
+++ app/ui/sidebar.tsx
@@ -3,7 +3,7 @@ export default function Sidebar() {
     <div className="col-span-2 p-4 border rounded shadow-xl hover:bg-sky-200">
     <nav>
       <a href="/">Home</a> <br></br>
-      <a href="/lewin-portfolio/projects/">Projects</a> <br></br>
+      <a href="/lewin-portfolio/projects">Projects</a> <br></br>
       {/*<a href="/resume/">Resume</a> <br></br>
       <a href="/contact/">Contact Me</a> */}
     </nav>
